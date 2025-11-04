f08ef12abc3ffc28dfc70b21d06cffd5a081e2e4
LcsToti <165573482+LcsToti@users.noreply.github.com>
2025-11-03 22:41:10 -0300

    Feat: Add HabitumAPI to projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,21 +2,11 @@ import { ProjectItem } from "@/types/ProjectItemTypes";
 import pratododiaimg from "@/assets/projectImages/pratododia.jpg";
 import pccertoimg from "@/assets/projectImages/pccerto.jpg";
 import myportfolioimg from "@/assets/projectImages/myportfolio.jpg";
+import habitumapiimg from "@/assets/projectImages/habitumapi.jpg";
 
 export const projects: ProjectItem[] = [
   {
     id: "1",
-    title: "Prato Do Dia",
-    description:
-      "Um site de receitas de comida, com seção de comentários, filtros alimentares e muito mais.",
-    imgs: [pratododiaimg],
-    stack: "ASP.NET MVC, Bootstrap, Azure, HTML, CSS e JS",
-    gitRepo:
-      "https://github.com/LcsToti/PratoDoDia?tab=readme-ov-file",
-      access: "https://pratododia.lcstoti.dev",
-  },
-  {
-    id: "2",
     title: "PC Certo",
     description: "Um site de peças de hardware de computadores",
     imgs: [pccertoimg],
@@ -26,11 +16,32 @@ export const projects: ProjectItem[] = [
     access: "https://pc-certo.lcstoti.dev",
   },
   {
+    id: "2",
+    title: "Prato Do Dia",
+    description:
+      "Um site de receitas de comida, com seção de comentários, filtros alimentares e muito mais.",
+    imgs: [pratododiaimg],
+    stack: ".NET MVC, Bootstrap, Azure, HTML, CSS e JS",
+    gitRepo:
+      "https://github.com/LcsToti/PratoDoDia?tab=readme-ov-file",
+      access: "https://pratododia.lcstoti.dev",
+  },
+  {
     id: "3",
+    title: "HabitumAPI",
+    description: "API para um aplicativo de gestão de hábitos gamificado.",
+    imgs: [habitumapiimg],
+    stack: "C#, .NET",
+    gitRepo:
+      "https://github.com/LcsToti/HabitumAPI",
+    access: "https://habitumapi.lcstoti.dev/",
+  },
+  {
+    id: "4",
     title: "My Portfolio",
     description: "Este site que você está vendo agora!",
     imgs: [myportfolioimg],
-    stack: "React, TypeScript, Tailwind CSS, Vite",
+    stack: "React, TypeScript, Tailwind CSS, Vite, Nginx, Oracle",
     gitRepo:
       "https://github.com/LcsToti/MyPortfolio?tab=readme-ov-file",
     access: "https://lcstoti.dev/",
